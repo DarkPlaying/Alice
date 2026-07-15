e37af618411dc304a2bcdc70791779585607d95a
DarkPlaying <sanjayappa230506@gmail.com>
2026-07-15 20:25:40 +0530

    Enable glitch effect on game cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ export default {
             },
             animation: {
                 "meteor-effect": "meteor 5s linear infinite",
+                "glitch": "glitch 0.3s cubic-bezier(.25, .46, .45, .94) both infinite",
             },
             keyframes: {
                 meteor: {
@@ -33,6 +34,14 @@ export default {
                         transform: "rotate(215deg) translateX(-500px)",
                         opacity: "0",
                     },
+                },
+                glitch: {
+                    '0%': { transform: 'translate(0)' },
+                    '20%': { transform: 'translate(-2px, 2px)' },
+                    '40%': { transform: 'translate(-2px, -2px)' },
+                    '60%': { transform: 'translate(2px, 2px)' },
+                    '80%': { transform: 'translate(2px, -2px)' },
+                    '100%': { transform: 'translate(0)' },
                 },
             },
         },
